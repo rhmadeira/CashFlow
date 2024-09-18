63d2c921a8d02d8aa968266b3ed46b178e427f1a
Rafael Henrique Madeira Feitosa <rafael.henrique.mad@gmail.com>
2024-09-18 10:52:08 -0300

    Add ExpensesRepository and update CashFlowDbContext visibility

--- src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
+++ src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -3,7 +3,7 @@ using CashFlow.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace CashFlow.Infrastructure.DataAccess;
-public class CashFlowDbContext : DbContext
+internal class CashFlowDbContext : DbContext
 {
     public DbSet<Expense> Expenses { get; set; }
 
