5c7cf0d0b5cdbf520c9d31956c979159e7e43bc3
Rafael Henrique Madeira Feitosa <rafael.henrique.mad@gmail.com>
2024-10-08 11:19:50 -0300

    Add PDF report generation and update report endpoints
    - Added a new endpoint in `ReportController.cs` for generating PDF reports.
    - Modified the existing report endpoint to return Excel files instead of PDFs.
    - Updated `CashFlow.Application.csproj` to embed required font files for PDF generation.
    - Included `GenerateExpensesReportPdfUseCase` in the dependency injection container in `DependencyInjectionExtension.cs`.
    - Added `GenerateExpensesReportPdfUseCase` class to handle PDF report generation.
    - Added `IGenerateExpensesReportPdfUseCase` interface to define the contract for PDF report generation.

--- src/CashFlow.Api/Controllers/ReportController.cs
+++ src/CashFlow.Api/Controllers/ReportController.cs
@@ -1,4 +1,5 @@
 ﻿using CashFlow.Application.UseCases.Expenses.Reports.Excel;
+using CashFlow.Application.UseCases.Expenses.Reports.Pdf;
 using Microsoft.AspNetCore.Mvc;
 using System.Net.Mime;
 
@@ -23,6 +24,25 @@ public class ReportController : ControllerBase
             return NoContent();
         }
 
-        return File(file, MediaTypeNames.Application.Octet, "report.pdf");
+        return File(file, MediaTypeNames.Application.Octet, "report.xlsx");
+    }
+
+    [HttpGet("pdf")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+
+    public async Task<IActionResult> GetPdf(
+        [FromHeader] DateOnly period,
+        [FromServices] IGenerateExpensesReportPdfUseCase useCase
+        )
+    {
+        byte[] file = await useCase.Execute(period);
+
+        if (file.Length == 0)
+        {
+            return NoContent();
+        }
+
+        return File(file, MediaTypeNames.Application.Pdf, "report.pdf");
     }
 }
---
+++ src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/GenerateExpensesReportPdfUseCase.cs
@@ -0,0 +1,26 @@
+﻿
+using CashFlow.Domain.Repositories.Expenses;
+
+namespace CashFlow.Application.UseCases.Expenses.Reports.Pdf;
+
+public class GenerateExpensesReportPdfUseCase : IGenerateExpensesReportPdfUseCase
+{
+    private readonly IExpensesReadOnlyRepository _expenseRepository;
+
+    public GenerateExpensesReportPdfUseCase(IExpensesReadOnlyRepository expenseRepository)
+    {
+        _expenseRepository = expenseRepository;
+    }
+
+    public async Task<byte[]> Execute(DateOnly period)
+    {
+       var exepenses = await _expenseRepository.GetByPeriod(period);
+
+        if(exepenses.Count == 0)
+        {
+            return [];
+        }
+
+        return [];
+    }
+}
